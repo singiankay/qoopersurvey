24cd392340b342c8c0100b66ed5fe96d71dc0b19
Kay Singian <singiankay@gmail.com>
2024-04-27 01:05:03 +0800

    chore: dont mind

--- src/main.ts
+++ src/main.ts
@@ -30,7 +30,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(PORT, () => {
+  await app.listen(PORT, HOST, () => {
     console.log(`Listening on PORT ${PORT} and HOST ${HOST}`);
   });
 }
